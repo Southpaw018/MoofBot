4cc3ae86ddb04fbea7ec2c6f231f61b39c5d2023
Jim Drey <moof@moofit.com>
2016-11-28 00:44:54 -0500

    Initial commit
    Is this thing on?

---
+++ moofbot.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+
+const token = JSON.parse(fs.readFileSync('token.json', 'utf8')).token;
+
+bot.on('ready', () => {
+    console.log(`MoofBot ready. Logged in as ${client.user.username}#${client.user.discriminator}`);
+});
+
+bot.on('message', msg => {
+    if(msg.channel.type === 'dm') return;
+    if (msg.content != null && !msg.author.bot) {
+        if (msg.content.charAt(0) == '.') { //prefix check
+            while (msg.content.charAt(0) === '.') { //remove prefix character(s)
+                msg.content = msg.content.substr(1);
+            }
+
+            if (msg.content === "yt") {
+                msg.channel.sendMessage("Hello, world!");
+            }
+        }
+    }
+});
+
+bot.login(token);
